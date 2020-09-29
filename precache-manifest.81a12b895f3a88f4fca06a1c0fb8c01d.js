382300076a43399212d96a441a473793fe2a8d36
Daniel Gerrald <daniel.gerrald@icloud.com>
2020-09-29 09:50:10 -0400

    Updates

---
+++ precache-manifest.81a12b895f3a88f4fca06a1c0fb8c01d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0b3ee8d85d01afb6f27f0e89651f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "717411e0bc10160e58bd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61ecaf7dc6b08af1dfbf",
+    "url": "/static/js/2.40539363.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40539363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717411e0bc10160e58bd",
+    "url": "/static/js/main.15145630.chunk.js"
+  },
+  {
+    "revision": "68056d2d3b9231ac4a59",
+    "url": "/static/js/runtime-main.3be91cb3.js"
+  }
+]);
